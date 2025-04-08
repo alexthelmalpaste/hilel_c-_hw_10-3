abfec82149421179571acdb28bf097b781e61ea2
alexthelmalpaste <sasaglazov222@gmail.com>
2025-04-08 16:53:14 +0300

    first-comm

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть рядок зі словами, розділеними комами: ");
+        string input = Console.ReadLine();
+
+        string cleanedString = input.Replace(" ", "");
+
+        Console.WriteLine($"Результат: {cleanedString}");
+    }
+}
